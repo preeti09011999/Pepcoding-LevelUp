3b95daab899cbfc455a1dfbd8ceb27d7fc5b9918
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-07-19 17:49:31 +0530

    Sort an array of elements 0,1,2
    Three way partioning

---
+++ Sorting/sort012.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void sort012(int[] arr){
+    //write your code here
+    
+  }
+
+  // used for swapping ith and jth elements of array
+  public static void swap(int[] arr, int i, int j) {
+    System.out.println("Swapping index " + i + " and index " + j);
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static void print(int[] arr){
+    for(int i = 0 ; i < arr.length; i++){
+      System.out.println(arr[i]);
+    }
+  }
+  public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ;i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    sort012(arr);
+    print(arr);
+  }
+
+}
