86d6e5f4756c28366687d8fa3abc5dede2b900af
Preeti <46680479+preeti09011999@users.noreply.github.com>
2020-08-22 21:36:10 +0530

    Update binaryTreeToCDLL.java

--- tree/binaryTreeToCDLL.java
+++ tree/binaryTreeToCDLL.java
@@ -1,9 +1,34 @@
-if(root == null) return null;
-        bTreeToClist(root.left);
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+*/
+class Tree
+{ 
+    Node head = null;
+    Node prev = null;
+    Node bTreeToClist(Node root)
+    {
+        bTreeToClist_(root);
+        prev.right = head;
+        head.left = prev;
+        return head;
+    }
+    public void bTreeToClist_(Node root){
+        if(root == null) return;
+        bTreeToClist_(root.left);
         if(head == null) head = root;
         else{
             root.left = prev;
             prev.right = root;
         }
         prev = root;
-        bTreeToClist(root.right);
+        bTreeToClist_(root.right);
+    }
+    
+}
+    
